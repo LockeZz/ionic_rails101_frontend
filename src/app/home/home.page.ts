4b215ce4adfffd7742ada2ed4e5171cd8e8d4694
LockeZz <LockeZz.PHF@gmail.com>
2021-06-08 01:05:11 +0800

    ionic-angular rails worked

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { HTTP } from '@ionic-native/http/ngx';
+import { HttpClient } from '@angular/common/http';
+import { Name } from './name.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  names: Name[];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.listName();
+  }
+
+  listName() {
+    return this.http
+      .get<any>('http://localhost:3000/api/people')
+      .subscribe((data) => {
+        console.log(data);
+        this.names = data;
+      });
+  }
 }
---
+++ src/app/home/name.model.ts
@@ -0,0 +1,11 @@
+export class Name {
+  public id: number;
+  public name: string;
+  public phone: string;
+
+  constructor(id: number, name: string, phone: string) {
+    this.id = id;
+    this.name = name;
+    this.phone = phone;
+  }
+}
